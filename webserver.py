4ca58ee5d23af1acae7e8d92ba5e28fe517be944
ahammer0 <axel.schwindenhammer@gmail.com>
2024-12-27 15:01:47 +0100

    add ajax updates

--- webserver.py
+++ webserver.py
@@ -14,8 +14,25 @@ def webserver(dbLock):
 
         return render_template('index.html', state=state, currentPeriode=currentPeriode,periodes=allPeriods)
 
+    @app.route('/toggle', methods=['POST'])
+    def toggleWorking():
+        db = DbAccess(dbLock)
+        db.toggleWorking()
+        if db.isActivePeriode:
+            return f"""<p>Actif depuis: {db.currentPeriode.getStartTimeStr()}</p>"""
+        else:
+            return """<p>Inactif</p>"""
 
-    app.run()
+    @app.route('/getPeriodes', methods=['GET'])
+    def getPeriodes():
+        db = DbAccess(dbLock)
+        periodes = db.getAllPeriodes()
+        output =""
+        for periode in periodes:
+            output+=f"<li>{periode.getStartTimeStr()} - {periode.getDuration()}</li>"
+        return output
+
+    app.run(host='0.0.0.0', port=5000)
 
 if __name__ == '__main__':
     print("test webserver")
